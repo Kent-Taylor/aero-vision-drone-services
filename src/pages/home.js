635f5f15273d68b57e95ca53d8ffa702dd193756
107124 <kredfox4@gmail.com>
2023-07-02 17:25:43 -0600

    added home page

---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+// import './home.css';
+
+function home() {
+  return (
+    <div className="home">
+      <header className="home-header">
+        <h1>Home Page</h1>
+      </header>
+    </div>
+  );
+}
+
+export default home;
