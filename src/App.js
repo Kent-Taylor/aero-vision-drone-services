721d00536fb152e8984fa9c812e08e0b614ef258
107124 <kredfox4@gmail.com>
2023-07-25 08:25:16 -0400

    customized all page headers with fade, images, logos and header names

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import Navigation from "./components/Navigation.js";
 
 
 
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,21 @@
 // import logo from './logo.svg';
 import Navigation from '../components/Navigation';
+import { Fade, Slide } from "react-awesome-reveal";
+
 import '../styles/about.scss';
+import TransparentLogo from '../images/ariaLogoTransparent.png'
+
 
 function about() {
   return (
     <div className="about">
       <Navigation/>
-        <header className="about-header">
-        <h1>About</h1>
+      <header className="about-header">
+               <a href="/home" className="logo-link">
+                <img src={TransparentLogo} alt="aria germann counseling logo transparent png" />
+        </a>
+<Fade><h1 className='heading-name'>About</h1></Fade>
+
       </header>
     </div>
   );
