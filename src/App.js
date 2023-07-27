246d57b1d9528c091bd5e31371149d6795226ba0
107124 <kredfox4@gmail.com>
2023-07-27 09:37:02 -0400

    added seo on common and home page and worked on section one of the squares of home page

--- src/App.js
+++ src/App.js
@@ -12,11 +12,28 @@ import Connect from './pages/connect.js';
 
 
 import './styles/common.scss';
+// import './styles/home.scss';
+// import './styles/about.scss';
+// import './styles/location.scss';
+// import './styles/faq.scss';
+// import './styles/connect.scss';
+import './styles/header.scss';
+import './styles/navigation.scss';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import Navigation from "./components/Navigation.js";
+
+// HTML code
+// Here are some of the handiest semantic HTML5 elements, use them to improve your communication with search engines:
+
+// article - isolates a post from the rest of the code, makes it portable
+// section - isolates a group of posts within a blog or a group of headings within a post
+// aside - isolates supplementary content that is not part of the main content
+// header - isolates the top part of the document, article, section, may contain navigation
+// footer - isolates the bottom of the document, article, section, contains meta information
+// nav - isolates navigation menus, groups of navigational elements
 
 
 
@@ -26,7 +43,9 @@ function App() {
       <ParallaxProvider>
         <React.StrictMode>
           <Router>
-          <Navigation/>
+            <nav>
+              <Navigation/>
+            </nav>
             <Routes>
               <Route exact path='/' element={<Home />} />
               <Route path='/home' element={<Home />} />
--- src/pages/home.js
+++ src/pages/home.js
@@ -21,7 +21,28 @@ function home() {
         </Fade>
 
         </header>
-          {/* <BasicCard/> */}
+      <main className="home-main">
+        <section className="section-one">
+
+          <article className="left-square">
+            <header>
+              <h1>Together, we embark on a rewarding journey</h1>
+            </header>
+          <p>We support you as you create YOUR second story; a new and healthy narrative about your life. Second Story Counseling is committed to providing high-quality therapy to people in all stages:</p>
+          <ul>
+            <li>Individual Counseling for Adults</li>
+            <li>Individual Counseling for Adolescents</li>
+            <li>Individual Counseling for Children</li>
+            <li>Marriage Counseling</li>
+            <li>Premarital Counseling</li>
+            <li>DBT Groups for Adolescents and Adults</li>
+          </ul>
+          </article>
+              <div className="right-square">
+                right side
+              </div>
+        </section>
+          </main>
     </div>
   );
 }
