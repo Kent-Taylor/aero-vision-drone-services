783d310b3cbb4e551dab3b0f1f42a3d57591e03e
Kent <kredfox4@gmail.com>
2023-09-27 20:17:13 -0400

    added first section of location page

--- src/pages/location.js
+++ src/pages/location.js
@@ -1,6 +1,8 @@
 // import logo from './logo.svg';
 // import { Fade, Slide } from "react-awesome-reveal";
 import { Fade } from "react-awesome-reveal";
+import Fab from '@mui/material/Fab';
+import NavigationIcon from '@mui/icons-material/Navigation';
 
 import '../styles/location.scss';
 import TransparentLogo from '../images/ariaLogoTransparent.png'
@@ -8,16 +10,33 @@ import TransparentLogo from '../images/ariaLogoTransparent.png'
 
 function location() {
   return (
-    <div className="location">
+    <div className="location-page">
 
       <header className="location-header">
-                        <a href="/home" className="logo-link">
-                <img className='transparent-logo' src={TransparentLogo} alt="aria germann counseling logo transparent png" />
+        <a href="/home" className="logo-link">
+        <img className='transparent-logo' src={TransparentLogo} alt="aria germann counseling logo transparent png" />
         </a>
         <Fade>
           <h1 className='heading-name'>Location</h1>
 </Fade>
       </header>
+      <section className="location-section-one">
+        <div className="come-see-me-box">
+
+          <h1>Come & See Me!</h1>
+          <Fab variant="extended" color="primary">
+            <NavigationIcon sx={{ mr: 1 }} />
+              Map
+          </Fab>
+        </div>
+      </section>
+
+      <section className="location-section-two">
+        {/* <Fab variant="extended" color="primary">
+          <NavigationIcon sx={{ mr: 1 }} />
+            Map
+        </Fab> */}
+      </section>
     </div>
   );
 }
