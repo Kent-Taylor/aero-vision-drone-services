92c81de58b10c5b30d724f694a5f90fa18255c91
Kent <kredfox4@gmail.com>
2023-10-08 20:00:15 -0400

    completed the locations page and did lots of design refining for all completed pages for ultry wide screen sizes and mobile

--- src/pages/about.js
+++ src/pages/about.js
@@ -28,67 +28,57 @@ function about() {
         </div>
         <div className="right">
           <article>
-            <Fade duration={500}>
               <p>
                 <span className="first-letter">As a young teenager, I was going through an emotional struggle and my mother suggested therapy.</span>
               </p>
-            </Fade>
-            <Fade duration={500}>
 
             <p>
               “Me? Therapy? No way” I replied. “Therapy is only for people who have major issues!” She “insisted” that I give it a try, and I am grateful she did.  Speaking to a non-judgmental, compassionate person, whose main goal was to help me succeed, enabled me to quickly work through issues that may have lingered for years, and ultimately changed my life. I greatly admire and appreciate the therapist who helped me. She inspires me to give that same gift to my clients, with the same level of kindness and compassion, that I received.  I know that the idea of therapy can be intimidating. But, I promise, the steps you are taking can be life altering.
             </p>
-            </Fade>
 
           </article>
         </div>
         </section>
       <section className="section-two">
-      {/* <Fade> */}
+        <Fade duration={500}>
+          <div className="line"/>
+        </Fade>
 
             <p>
               Nothing makes me happier than empowering someone with the tools they need to be proactive in overcoming their current challenges, and I ensure this is done in a kind, compassionate and non-judgmental environment. I deeply care about my clients and understand the pain that brings them to counseling. My passion is helping adolescents and female adults/young adult females become the best them.
-          </p>
-          {/* </Fade> */}
-          {/* <Slide className="slide"> */}
-          {/* <Fade> */}
+            </p>
+        <Fade duration={500}>
+          <div className="line"/>
+        </Fade>
 
         <p className="big-text">
         I use a mix of CBT, ACT, DBT and a strengths-based approach during therapy.
           </p>
-          {/* </Fade> */}
-{/* </Slide> */}
       </section>
 
         <div className="aria-container-two">
           <div className="left">
-            {/* <Fade> */}
 
             <p>
             I have helped people work through feelings of depression, anxiety, perfectionism, trauma, burnout, grief or suicide.  I also love to help people explore issues of: self-worth/self-compassion/self-esteem, life transitions, family conflict, coping skills, codependency, anger management, relationship issues, boundaries, communication skills and emotional regulation.
             </p>
-            {/* </Fade> */}
-
         </div>
-          {/* <Fade> */}
           <div className="right">
-
               <img src={OwnerPic} className='owner-pic-2' alt="aria germann counseling profile" />
-
           </div>
-            {/* </Fade> */}
-
         </div>
           <div className="about-me-container">
         <Fade duration={500}>
           <div className="line"/>
+        </Fade>
 
             <p>
               A little about me: I am a Licensed Master Social Worker, and a recovering perfectionist. I was born and raised in the Bay Area.  I received an undergraduate degree in Human Development from Brigham Young University.  I also received my Masters Degree from Brigham University. When I’m not helping my clients to become the best they can be, you’ll find me reading, hiking, walking, playing pickleball, shopping or traveling.
           </p>
+        <Fade duration={500}>
           <div className="line"/>
-
         </Fade>
+
           </div>
 
 
@@ -96,10 +86,8 @@ function about() {
 
       <section className="section-three">
       <Fade duration={500}>
-<p>I love going hiking.</p>
-
+        <p>I love going hiking.</p>
           </Fade>
-
       </section>
       <section className="section-four">
       <Fade duration={500}>
